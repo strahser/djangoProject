3ca712583dc37a8e48f11a1c9fabc1e829b5d390
strahser <67167650+strahser@users.noreply.github.com>
2026-07-30 17:51:06 +0300

    router: system/third-party всегда default, PersonalData всегда personal_db, остальные переключаются

--- djangoProject/db_switch/DbRouter.py
+++ djangoProject/db_switch/DbRouter.py
@@ -1,9 +1,21 @@
 from .thread_local import get_current_db
 
+SWITCHABLE_APPS = {
+    'ProjectTDL', 'StaticData', 'ProjectContract',
+    'Emails', 'email_ui', 'TelegramParser',
+}
+
+PERSONAL_APPS = {'PersonalData'}
+
 
 class DatabaseSwitchRouter:
     def _route_to(self, model):
-        return get_current_db()
+        app = model._meta.app_label
+        if app in PERSONAL_APPS:
+            return 'personal_db'
+        if app in SWITCHABLE_APPS:
+            return get_current_db()
+        return 'default'
 
     def db_for_read(self, model, **hints):
         return self._route_to(model)
@@ -15,4 +27,8 @@ class DatabaseSwitchRouter:
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        return db == get_current_db()
+        if app_label in PERSONAL_APPS:
+            return db == 'personal_db'
+        if app_label in SWITCHABLE_APPS:
+            return db == get_current_db()
+        return db == 'default'
